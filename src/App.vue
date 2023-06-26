9000dec96532147ad80bc8a3e074b255d90a11be
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-06-26 14:22:59 +0200

    lesson

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,35 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
+  <router-view />
+  
+
+  <p><span :title="message">Hover & See the Dynamic Title</span></p>
+  <p :class="{ red: isRed }" @click="toggleRed()">
+    This should be red... But click me to toggle
+  </p>
+  <p :style="{ color }" @click="toggleColor()">
+    This will change color when clicking
+  </p>
 </template>
+
+<script>
+export default {
+   data() {
+    return { message: "Hello There", isRed: true, color: 'green' };
+  },
+
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed
+    },
+    toggleColor () {
+      this.color= this.color === 'green' ? 'blue' : 'green'
+    }
+  }
+
+}
+</script>
 
 <style>
 #app {
@@ -14,6 +41,10 @@
   text-align: center;
   color: #2c3e50;
 }
+
+ .red {
+    color: red;
+  }
 
 nav {
   padding: 30px;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,60 @@
 <template>
-  <div class="home">
+  <!-- <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </div> -->
+
+  <!-- <div>
+    <p v-if="hours<12">Good morning!</p>
+    <p v-if="hours>=12 && hours < 18">Good Afternoon!</p>
+    <p v-if="hours >=18">Good Evening!</p>
+  </div> -->
+
+  <!-- <div>
+    <p v-if="path === '/'">You are on the Home Page</p>
+    <p v-else>Youre on {{path}}</p>
+  </div> -->
+
+  <p><span :title="message">Hover & See the Dynamic Title</span></p>
+  <p :class="{ red: isRed }" @click="toggleRed">
+    This should be red... But click me to toggle
+  </p>
+  <p :style="{ color }" @click="toggleColor">This will change color when clicking</p>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+
+  data() {
+    return { message: "Hello There", isRed: true, color: green };
+  },
+
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed
+    },
+    toggleColor () {
+      this.color= this.color === 'green' ? 'blue' : 'green'
+    }
   }
-}
+
+  // name: 'HomeView',
+  // data: {},
+  // components: {
+  //   HelloWorld
+  // }
+  // data: { path: location.pathname },
+};
+// data: {
+//   hours: new Date().getHours();
+// }
 </script>
+
+<style>
+  red {
+    color: red;
+  }
+</style>
